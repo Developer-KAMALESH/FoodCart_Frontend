b6f18acb3291328331641a35500f234daddfab90
KAMALESH S <selvavinayagam944@gmail.com>
2026-03-15 13:01:35 +0530

    improved checkout UX

--- src/app/pages/checkout/checkout.ts
+++ src/app/pages/checkout/checkout.ts
@@ -2,16 +2,30 @@ import { Component } from '@angular/core';
 import { Cart } from '../../services/cart';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 
 @Component({
   selector: 'app-checkout',
-  imports: [CommonModule,RouterModule],
+  imports: [CommonModule,RouterModule,FormsModule],
   templateUrl: './checkout.html',
   styleUrl: './checkout.css',
 })
 export class Checkout {
   constructor(private cartService: Cart) {}
+
+  orderType = "DELIVERY";
+
+tableNumber = "";
+pickupTime = "";
+
+name = "";
+phone = "";
+email = "";
+
+address = "";
+city = "";
+pincode = "";
 
   get cartItems() {
     return this.cartService.getItems();
